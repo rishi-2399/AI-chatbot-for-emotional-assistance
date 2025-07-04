6aee625666cec03e8643f22c663795b418cdad7b
rishi-2399 <rishikesh.k2399@gmail.com>
2025-07-03 23:37:35 -0500

    Add files via upload

---
+++ attention_focal_loss.py
@@ -0,0 +1,36 @@
+
+import tensorflow as tf
+from tensorflow.keras.losses import Loss
+from tensorflow.keras.layers import Layer
+import tensorflow.keras.backend as K
+
+class Attention(Layer):
+    def __init__(self, **kwargs):
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.W = self.add_weight(name="att_weight", shape=(input_shape[-1], 1),
+                                 initializer="glorot_uniform", trainable=True)
+        self.b = self.add_weight(name="att_bias", shape=(input_shape[1], 1),
+                                 initializer="zeros", trainable=True)
+        super(Attention, self).build(input_shape)
+
+    def call(self, x):
+        e = K.tanh(K.dot(x, self.W) + self.b)
+        a = K.softmax(e, axis=1)
+        output = x * a
+        return K.sum(output, axis=1)
+
+class FocalLoss(Loss):
+    def __init__(self, gamma=2., alpha=0.25):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+
+    def call(self, y_true, y_pred):
+        epsilon = K.epsilon()
+        y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+        cross_entropy = -y_true * K.log(y_pred)
+        weight = self.alpha * K.pow(1 - y_pred, self.gamma)
+        loss = weight * cross_entropy
+        return K.sum(loss, axis=1)
